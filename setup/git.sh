6638936ae1c94ab9ecf42ea38c48cfef13563f5e
Js Lim <jslim89@gmail.com>
2015-04-17 22:10:54 +0800

    Removed unused file

--- setup/git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global color.ui auto
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.status auto
-git config --global alias.ll 'log --oneline --decorate --graph --all'
