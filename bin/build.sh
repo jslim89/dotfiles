b43aede9df3d8d4708afeb9f467863699ae67118
Js Lim <jslim89@gmail.com>
2015-04-17 21:42:23 +0800

    Updated instruction & installation script

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # installation
+sudo apt-get install git rubygems exuberant-ctags xclip mercurial vim tmux screen ack-grep
+sudo gem install homesick
 `which homesick` clone https://github.com/jslim89/dotfiles.git
 `which homesick` symlink dotfiles
 cd ~/.homesick/repos/dotfiles
+git checkout ubuntu
 git submodule update --init --recursive
-git checkout debian
 cd ~
 
 # Git config
