f7146069f5b9729358a68943ad4a6aadc9f93677
Js Lim <jslim89@gmail.com>
2015-04-17 18:11:32 +0800

    Added installation script

---
+++ bin/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# installation
+sudo apt-get install git rubygems exuberant-ctags xclip mercurial vim tmux screen ack-grep
+sudo gem install homesick
+`which homesick` clone https://github.com/jslim89/dotfiles.git
+`which homesick` symlink dotfiles
+cd ~/.homesick/repos/dotfiles
+git submodule update --init --recursive
+git checkout debian
+cd ~
+
+# Git config
+echo "Updating Git config..."
+git config --global color.ui auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.status auto
+git config --global alias.ll 'log --oneline --decorate --graph --all'
