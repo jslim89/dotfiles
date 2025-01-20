f0be1e678e382f93a7a8bb51761c1c3bddb94a41
Js Lim <jslim89@gmail.com>
2025-01-20 15:39:46 +0800

    Enable edit command in buffer

--- home/.zsh.d/init.zsh
+++ home/.zsh.d/init.zsh
@@ -5,3 +5,8 @@
 eval "$(atuin init zsh)"
 eval "$(zoxide init zsh)"
 
+# Ctrl+x Ctrl+e to edit command in buffer with $EDITOR
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
